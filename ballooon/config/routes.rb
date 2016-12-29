94ccd8c32f7d7acda93a08caa87345799a44a7d8
raul bolanos <bolanos_raul@hotmail.com>
2016-12-29 18:40:46 +0100

    implemented things: navigation and footer, four views implemented: home, gallery, about, contact. in pages is a corrousel implemented  with some articles

--- ballooon/config/routes.rb
+++ ballooon/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'pages#home'
+
+  get "gallery" => "pages#gallery", as: :gallery
+  get "about" => "pages#about", as: :about
+  get "contact" => "pages#contact", as: :contact
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
