e993fa8a0a8430043052b5604a6a6d3bcac12a0f
raul bolanos <bolanos_raul@hotmail.com>
2017-01-17 17:23:24 +0100

    not

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get "gallery" => "pages#gallery", as: :gallery
-  get "about" => "pages#about", as: :about
-  get "contact" => "pages#contact", as: :contact
+  get 'gallery', to: 'pages#gallery'
+  get 'about', to: 'pages#about'
+  get 'contact', to:  'pages#contact'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
